18d8c5b2aed7231998309799a6d69034eb5f0a53
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-12-30 16:08:11 +0100

    Update UnderConstruction.component.tsx

--- components/underConstruction/UnderConstruction.component.tsx
+++ components/underConstruction/UnderConstruction.component.tsx
@@ -7,14 +7,15 @@ export default function UnderConstruction() {
     <Link href="/" >
       <div className="w-screen h-screen bg-primary pt-80 hover:cursor-pointer">
         <div className="flex w-fit mx-auto place-items-center">
-          <Image src="/favicon.svg" alt="OzonMaskinen" width={50} height={50} />
+          <Image src="/favicon.svg" alt="Ozonbehandling" width={50} height={50} />
           <p className=" text-2xl lg:text-5xl 2xl:text-6xl mx-auto text-white pl-5">
-            OzonMaskinen.no
+          Ozonbehandling.no
           </p>
         </div>
 
-        <h1 className="text-xs lg:text-md 2xl:text-lg pt-20 lg:pt-5 2xl:pt-10 mx-auto w-fit text-white ">
-          This page is under construction
+        <h1 className="text-xs lg:text-md 2xl:text-lg pt-20 lg:pt-5 2xl:pt-10 mx-auto w-fit text-white text-center">
+          Denne siden er fortsatt under utvikling.<br/><br/>
+          <span className="underline underline-offset-4">TRYKK</span> for å gå tilbake til forsiden.
         </h1>
       </div>
     </Link>
