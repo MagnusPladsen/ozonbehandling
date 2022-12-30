4b6f49fa39bcdb3f431aa6ef103c5ee432c09830
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-12-30 12:57:20 +0100

    Update bedrift.tsx

--- pages/bedrift.tsx
+++ pages/bedrift.tsx
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import React, { useState } from "react";
-import NavBar from "../components/navigation/NavBar.component";
-import SiteWrapper from "../components/siteWrapper/SiteWrapper.component";
-import UnderConstruction from "../components/underConstruction/UnderConstruction.component";
+import { useState } from "react";
 import CompanyComponent from "../components/company/Company.component";
 import CompanyHero from "../components/company/CompanyHero.component";
 import Footer from "../components/footer/Footer.component";
+import NavBar from "../components/navigation/NavBar.component";
+import SiteWrapper from "../components/siteWrapper/SiteWrapper.component";
 
 export default function Company() {
   const [dropdownOpen, setdropdownOpen] = useState(false);
@@ -21,14 +20,19 @@ export default function Company() {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500&display=swap"
           rel="stylesheet"
         />
-        <meta name="description" content="Ozonbehandling for bedrifter. Vi tilbyr ozonbehandling for bedrifter som ønsker å redusere smittefare og øke hygienen." />
-        <meta name="keywords" content="Ozonbehandling, lukt, hotell, hybel, leilighet, camping, hytte, hus, bil, bobil"/>
+        <meta
+          name="description"
+          content="Ozonbehandling for bedrifter. Vi tilbyr ozonbehandling for bedrifter som ønsker å redusere smittefare og øke hygienen."
+        />
+        <meta
+          name="keywords"
+          content="Ozonbehandling, lukt, hotell, hybel, leilighet, camping, hytte, hus, bil, bobil"
+        />
         <meta name="author" content="Mangus Pladsen" />
-
       </Head>
       <NavBar dropdownOpen={dropdownOpen} setdropdownOpen={setdropdownOpen} />
       <CompanyHero />
-      <SiteWrapper className={blurIfDropdownOpen} >
+      <SiteWrapper>
         <CompanyComponent />
       </SiteWrapper>
       <Footer />
